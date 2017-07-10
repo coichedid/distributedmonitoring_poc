182f5404447f1df7f401d0ffbba8d38e00f43f70
Clovis Chedid <coichedid@gmail.com>
2017-07-10 00:39:05 -0300

    PoC with java end nodejs services

--- run_docker_local.sh
+++ run_docker_local.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #echo "Loading elastic search container"
-#docker run -d -it --name es -p 9200:9200 -p 9300:9300 elasticsearch
+docker run -d -it --name es -p 9200:9200 -p 9300:9300 elasticsearch
 #echo "Loading kibana container"
-#docker run -d -it --name kibana --link es:elasticsearch -p 5601:5601 kibana
+docker run -d -it --name kibana --link es:elasticsearch -p 5601:5601 kibana
 #echo "Loading logstash container"
-#docker run -d -it --name logstash --link es:elasticsearch -p 5000:5000 logstash -e 'input { tcp { port => 5000 codec => "json" } } output { elasticsearch { hosts => ["elasticsearch"] index => "log-services-%{serviceName}"} }'
+docker run -d -it --name logstash --link es:elasticsearch -p 5000:5000 logstash -e 'input { tcp { port => 5000 codec => "json" } } output { elasticsearch { hosts => ["elasticsearch"] index => "log-services-%{serviceName}"} }'
 #echo "Loading zipkin container"
-#docker run -d -it -e STORAGE_TYPE='elasticsearch' -e ES_HOSTS='http://elasticsearch:9200' -e ES_INDEX='log-zipkin' --name zipkin --link es:elasticsearch -p 9411:9411 openzipkin/zipkin
+docker run -d -it -e STORAGE_TYPE='elasticsearch' -e ES_HOSTS='http://elasticsearch:9200' -e ES_INDEX='log-zipkin' --name zipkin --link es:elasticsearch -p 9411:9411 openzipkin/zipkin
 echo "Loading javaservice1"
 docker run -d -it -p 2222:2222 -h javaservice1 --name javaservice1 --link zipkin:zipkinservice --link logstash -t pocdistributedmonitoring/pocjavaservice1
 echo "Loading javaservice2"
